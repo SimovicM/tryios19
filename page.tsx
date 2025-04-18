bf0ccafc9e24c9d64ac2d915a210dfd5c7a346ef
Martin Šimovič <simovic.martin@icloud.com>
2025-04-18 18:56:49 +0200

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-import HomeScreen from './components/HomeScreen'
-
-export default function Home() {
-  return (
-    <main className="h-screen w-screen bg-[#000000] overflow-hidden">
-      <HomeScreen />
-    </main>
-  )
-} 
